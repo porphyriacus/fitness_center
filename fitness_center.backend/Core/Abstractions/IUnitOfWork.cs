5f67f478bd70e0bea9f0aa16d26018ca0e3d4659
porphyriacus <porph@example.com>
2026-05-26 13:16:58 +0300

    add interfaces: IUnitOfWork IRepository<T>

---
+++ fitness_center.backend/Core/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Abstractions
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Client> Clients { get; }
+        IRepository<Trainer> Trainers { get; }
+
+        IRepository<Booking> Bookings { get; }
+
+        IRepository<Membership> Memberships { get; }
+        IRepository<MembershipType> MembershipTypes { get; }
+
+        IRepository<Workout> Workouts { get; }
+        IRepository<WorkoutType> WorkoutTypes { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+        Task<int> CommitAsync(CancellationToken cancellationToken = default);
+        Task RollbackAsync(CancellationToken cancellationToken = default);
+    }
+}
