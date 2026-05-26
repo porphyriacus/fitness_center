e898d359036693ef62af548825aee9d81e3b3003
porphyriacus <porph@example.com>
2026-05-26 19:35:30 +0300

    Infrastracture: add DependencyInjection

--- fitness_center.backend/Infrastructure/DependencyInjection.cs
+++ fitness_center.backend/Infrastructure/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,14 @@ using System.Threading.Tasks;
 
 namespace Infrastructure
 {
-    internal class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, EfUnitOfWork>();
+            return services;
+        }
     }
+
 }
